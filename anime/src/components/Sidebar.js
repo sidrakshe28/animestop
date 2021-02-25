799eb3ffcf40f84cb9184441f7c3b43ab92f5a88
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-02-25 12:32:47 +0530

    added sidebar component

---
+++ anime/src/components/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <aside>
+            <nav>
+                <h3>TOP ANIME </h3>
+                <a href="" 
+                target="_blank" 
+                rel="noreferrer">
+                    Attack on titan
+                </a>
+
+                <a href="" 
+                target="_blank" 
+                rel="noreferrer">
+                    One Punch Man
+                </a>
+
+                <a href="" 
+                target="_blank" 
+                rel="noreferrer">
+                   Sword Art Online
+                </a>
+
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar
