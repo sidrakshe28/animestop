b7f0d2d28eb41f836b258180bd584f60d054bcce
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-02-25 12:35:48 +0530

    added maincontent component

--- anime/src/App.js
+++ anime/src/App.js
@@ -1,11 +1,19 @@
 
 
-//import react  from 'react';
+import { useState,useEffect } from 'react';
 import Header from './components/Header';
 function App() {
+
+  const [animeList,SetAnimeList] = useState([]);
+  const [topAnime,SetTopAnime] = useState([]);
+  const [search,setSearch] = useState(" ");
+
   return (
     <div className="App">
       <Header/>
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        </div>
       </div>
   );
 
---
+++ anime/src/components/MainContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function MainContent() {
+    return (
+       <main>
+           <div className="main-head">
+               <form className="search--box">
+                   <input type="search" placeholder="Search for an anime..." required />
+               </form>
+           </div>
+       </main>
+    )
+}
+
+export default MainContent
