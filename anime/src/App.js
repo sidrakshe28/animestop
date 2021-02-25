87af52f0984ba08dd6f81bb2c94193a9b4a3414f
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-02-25 12:37:57 +0530

    fetched jikan api

--- anime/src/App.js
+++ anime/src/App.js
@@ -2,17 +2,32 @@
 
 import { useState,useEffect } from 'react';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 function App() {
 
   const [animeList,SetAnimeList] = useState([]);
   const [topAnime,SetTopAnime] = useState([]);
   const [search,setSearch] = useState(" ");
 
+  const GetTopAnime = async() =>{
+    const temp =await fetch(`https://api.jikan.moe/v3/top/anim/1/bypopularity`)
+    .then(res=> res.json());
+
+    SetTopAnime(temp.top.slice(0,5));
+
+  }
+  useEffect( () => {
+    GetTopAnime();
+
+    //console.log("Top Anime");
+  } ,[]);
+
   return (
     <div className="App">
       <Header/>
       <div className="content-wrap">
         <Sidebar topAnime={topAnime} />
+        <MainContent/>
         </div>
       </div>
   );
